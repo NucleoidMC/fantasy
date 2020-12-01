736943528f9e53b97e0315bd4dc0e06dd71b330b
gegy1000 <gegy1000@gmail.com>
2020-12-01 16:38:41 +0200

    catch error and disconnect player when failing to transfer from/into bubble world

--- src/main/java/xyz/nucleoid/fantasy/BubbleWorld.java
+++ src/main/java/xyz/nucleoid/fantasy/BubbleWorld.java
@@ -6,6 +6,7 @@ import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.LiteralText;
 import net.minecraft.util.ProgressListener;
 import net.minecraft.util.Util;
 import net.minecraft.util.math.BlockPos;
@@ -123,10 +124,15 @@ final class BubbleWorld extends ServerWorld {
 
     void kickPlayer(ServerPlayerEntity player) {
         if (!this.removeBubblePlayer(player, true) || player.world == this) {
-            ServerWorld overworld = this.getServer().getOverworld();
-            BlockPos spawnPos = overworld.getSpawnPos();
-            float spawnAngle = overworld.getSpawnAngle();
-            player.teleport(overworld, spawnPos.getX() + 0.5, spawnPos.getY(), spawnPos.getZ() + 0.5, spawnAngle, 0.0F);
+            try {
+                ServerWorld overworld = this.getServer().getOverworld();
+                BlockPos spawnPos = overworld.getSpawnPos();
+                float spawnAngle = overworld.getSpawnAngle();
+                player.teleport(overworld, spawnPos.getX() + 0.5, spawnPos.getY(), spawnPos.getZ() + 0.5, spawnAngle, 0.0F);
+            } catch (Exception e) {
+                player.networkHandler.disconnect(new LiteralText("Failed to transfer from world!"));
+                Fantasy.LOGGER.error("Failed to kick from bubble world", e);
+            }
         } else {
             this.notifyRemovePlayer(player);
         }
@@ -147,37 +153,47 @@ final class BubbleWorld extends ServerWorld {
     boolean removeBubblePlayer(ServerPlayerEntity player, boolean immediate) {
         this.assertServerThread();
 
-        PlayerSnapshot snapshot = this.players.remove(player.getUuid());
-        if (snapshot != null) {
-            this.notifyRemovePlayer(player);
+        try {
+            PlayerSnapshot snapshot = this.players.remove(player.getUuid());
+            if (snapshot != null) {
+                this.notifyRemovePlayer(player);
 
-            if (immediate) {
-                snapshot.restore(player);
-            } else {
-                // this might be called from a player being teleported out of the dimension
-                // in that case, we don't want to recursively teleport: wait for next tick to restore
-                this.fantasy.enqueueNextTick(() -> {
+                if (immediate) {
                     snapshot.restore(player);
-                });
-            }
+                } else {
+                    // this might be called from a player being teleported out of the dimension
+                    // in that case, we don't want to recursively teleport: wait for next tick to restore
+                    this.fantasy.enqueueNextTick(() -> {
+                        snapshot.restore(player);
+                    });
+                }
 
-            return true;
+                return true;
+            }
+        } catch (Exception e) {
+            player.networkHandler.disconnect(new LiteralText("Failed to transfer from world!"));
+            Fantasy.LOGGER.error("Failed to remove player from bubble world", e);
         }
 
         return false;
     }
 
     private void joinBubblePlayer(ServerPlayerEntity player) {
-        player.inventory.clear();
-        player.getEnderChestInventory().clear();
+        try {
+            player.inventory.clear();
+            player.getEnderChestInventory().clear();
 
-        player.setHealth(20.0F);
-        player.getHungerManager().setFoodLevel(20);
+            player.setHealth(20.0F);
+            player.getHungerManager().setFoodLevel(20);
 
-        Fantasy.resetPlayer(player);
+            Fantasy.resetPlayer(player);
 
-        player.setGameMode(this.config.getDefaultGameMode());
-        this.config.getSpawner().spawnPlayer(this, player);
+            player.setGameMode(this.config.getDefaultGameMode());
+            this.config.getSpawner().spawnPlayer(this, player);
+        } catch (Exception e) {
+            player.networkHandler.disconnect(new LiteralText("Failed to transfer into world!"));
+            Fantasy.LOGGER.error("Failed to join player into bubble world", e);
+        }
     }
 
     private void notifyAddPlayer(ServerPlayerEntity player) {
