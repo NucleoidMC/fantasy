36e85337461e9bc0c6d79057566a49f26bc64d3a
Gegy <gegy1000@gmail.com>
2021-05-07 14:41:59 +0200

    clear player attribute modifiers when resetting

--- src/main/java/xyz/nucleoid/fantasy/player/PlayerResetter.java
+++ src/main/java/xyz/nucleoid/fantasy/player/PlayerResetter.java
@@ -1,7 +1,12 @@
 package xyz.nucleoid.fantasy.player;
 
+import net.minecraft.entity.attribute.AttributeContainer;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeInstance;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.registry.Registry;
 
 public final class PlayerResetter {
     private final CompoundTag resetTag;
@@ -11,8 +16,28 @@ public final class PlayerResetter {
     }
 
     public void apply(ServerPlayerEntity player) {
-        player.fromTag(this.resetTag);
+        this.clearAttributeModifiers(player);
         player.clearStatusEffects();
         player.getScoreboardTags().clear();
+
+        player.fromTag(this.resetTag);
+    }
+
+    private void clearAttributeModifiers(ServerPlayerEntity player) {
+        AttributeContainer attributes = player.getAttributes();
+        for (EntityAttribute attribute : Registry.ATTRIBUTE) {
+            if (attributes.hasAttribute(attribute)) {
+                EntityAttributeInstance instance = attributes.getCustomInstance(attribute);
+                if (instance != null) {
+                    this.clearModifiers(instance);
+                }
+            }
+        }
+    }
+
+    private void clearModifiers(EntityAttributeInstance instance) {
+        for (EntityAttributeModifier modifier : instance.getModifiers()) {
+            instance.removeModifier(modifier);
+        }
     }
 }
