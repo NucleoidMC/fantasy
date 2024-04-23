a94afb95d7fe592c03e39e682d5130b3a4d853ad
Awakened-Redstone <40528665+Awakened-Redstone@users.noreply.github.com>
2024-04-22 23:59:00 -0300

    chore: Port to 1.20.5 and probably break something important

---
+++ src/main/java/xyz/nucleoid/fantasy/mixin/registry/DimensionOptionsRegistryHolderMixin.java
@@ -0,0 +1,30 @@
+package xyz.nucleoid.fantasy.mixin.registry;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.registry.Registry;
+import net.minecraft.world.dimension.DimensionOptions;
+import net.minecraft.world.dimension.DimensionOptionsRegistryHolder;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import xyz.nucleoid.fantasy.FantasyDimensionOptions;
+import xyz.nucleoid.fantasy.util.FilteredRegistry;
+
+import java.util.function.Function;
+
+@Mixin(DimensionOptionsRegistryHolder.class)
+public class DimensionOptionsRegistryHolderMixin {
+    @Unique private static final Logger fantasy$LOGGER = LogUtils.getLogger();
+
+    @ModifyArg(method = "method_45516", at = @At(value = "INVOKE", target = "Lcom/mojang/serialization/MapCodec;forGetter(Ljava/util/function/Function;)Lcom/mojang/serialization/codecs/RecordCodecBuilder;"))
+    private static Function<Object, Registry<DimensionOptions>> fantasy$swapRegistryGetter(Function<Object, Registry<DimensionOptions>> getter) {
+        try {
+            return (x) -> new FilteredRegistry<>(getter.apply(x), FantasyDimensionOptions.SAVE_PROPERTIES_PREDICATE);
+        } catch (Throwable e) {
+            fantasy$LOGGER.error("Failed method_45516", e);
+            throw e;
+        }
+    }
+}
