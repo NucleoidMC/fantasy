37467f158de06d3c35a7a0593e6480efd03fec9b
Awakened-Redstone <40528665+Awakened-Redstone@users.noreply.github.com>
2024-04-24 23:08:59 -0300

    chore: Finally get this mixin to work, mod compat unknown
    Signed-off-by: Awakened-Redstone <40528665+Awakened-Redstone@users.noreply.github.com>

--- src/main/java/xyz/nucleoid/fantasy/mixin/registry/SimpleRegistryMixin.java
+++ src/main/java/xyz/nucleoid/fantasy/mixin/registry/SimpleRegistryMixin.java
@@ -1,37 +1,43 @@
 package xyz.nucleoid.fantasy.mixin.registry;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.mojang.logging.LogUtils;
 import it.unimi.dsi.fastutil.objects.ObjectList;
 import it.unimi.dsi.fastutil.objects.Reference2IntMap;
+import net.minecraft.registry.MutableRegistry;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.SimpleRegistry;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.registry.entry.RegistryEntryInfo;
+import net.minecraft.registry.entry.RegistryEntryOwner;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
 import xyz.nucleoid.fantasy.RemoveFromRegistry;
 
 import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Stream;
 
 @Mixin(SimpleRegistry.class)
-public abstract class SimpleRegistryMixin<T> implements RemoveFromRegistry<T> {
+public abstract class SimpleRegistryMixin<T> implements RemoveFromRegistry<T>, MutableRegistry<T> {
     @Unique private static final Logger fantasy$LOGGER = LogUtils.getLogger();
+
     @Shadow @Final private Map<T, RegistryEntry.Reference<T>> valueToEntry;
     @Shadow @Final private Map<Identifier, RegistryEntry.Reference<T>> idToEntry;
     @Shadow @Final private Map<RegistryKey<T>, RegistryEntry.Reference<T>> keyToEntry;
     @Shadow @Final private Map<RegistryKey<T>, RegistryEntryInfo> keyToEntryInfo;
     @Shadow @Final private ObjectList<RegistryEntry.Reference<T>> rawIdToEntry;
     @Shadow @Final private Reference2IntMap<T> entryToRawId;
-    @Shadow private boolean frozen;
-
-    //@Shadow @Nullable private List<RegistryEntry.Reference<T>> cachedEntries;
-
     @Shadow @Final RegistryKey<? extends Registry<T>> key;
+    @Shadow private boolean frozen;
 
     @Override
     public boolean fantasy$remove(T entry) {
@@ -72,5 +78,10 @@ public abstract class SimpleRegistryMixin<T> implements RemoveFromRegistry<T> {
     @Override
     public boolean fantasy$isFrozen() {
         return this.frozen;
+    }
+
+    @ModifyReturnValue(method = "streamEntries", at = @At("RETURN"))
+    public Stream<RegistryEntry.Reference<T>> fixEntryStream(Stream<RegistryEntry.Reference<T>> original) {
+        return original.filter(Objects::nonNull);
     }
 }
