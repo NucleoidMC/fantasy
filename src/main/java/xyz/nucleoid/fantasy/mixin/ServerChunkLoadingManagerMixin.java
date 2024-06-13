4c25e02a76f593867cdbef00c6bd12d5d61907e3
LCLP <lukas@lclpnet.work>
2024-06-13 21:22:05 +0200

    fix runtime world chunk generator crash

--- src/main/java/xyz/nucleoid/fantasy/mixin/ServerChunkLoadingManagerMixin.java
+++ src/main/java/xyz/nucleoid/fantasy/mixin/ServerChunkLoadingManagerMixin.java
@@ -2,23 +2,20 @@ package xyz.nucleoid.fantasy.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.server.world.ServerChunkLoadingManager;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.gen.chunk.ChunkGeneratorSettings;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import xyz.nucleoid.fantasy.util.ChunkGeneratorSettingsProvider;
 
 @Mixin(ServerChunkLoadingManager.class)
-public abstract class ServerChunkLoadingManagerMixin {
-
-    @Shadow
-    public abstract ChunkGenerator getChunkGenerator();
+public class ServerChunkLoadingManagerMixin {
 
     @WrapOperation(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/gen/chunk/ChunkGeneratorSettings;createMissingSettings()Lnet/minecraft/world/gen/chunk/ChunkGeneratorSettings;"))
-    private ChunkGeneratorSettings fantasy$useProvidedChunkGeneratorSettings(Operation<ChunkGeneratorSettings> original) {
-    	if (this.getChunkGenerator() instanceof ChunkGeneratorSettingsProvider provider) {
+    private ChunkGeneratorSettings fantasy$useProvidedChunkGeneratorSettings(Operation<ChunkGeneratorSettings> original, @Local(argsOnly = true) ChunkGenerator chunkGenerator) {
+    	if (chunkGenerator instanceof ChunkGeneratorSettingsProvider provider) {
             ChunkGeneratorSettings settings = provider.getSettings();
             if (settings != null) return settings;
         }
