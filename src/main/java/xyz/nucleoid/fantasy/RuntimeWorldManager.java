09a9592adf6bfa2240d7390bf390cb1f3860f492
Patbox <39821509+Patbox@users.noreply.github.com>
2025-03-12 00:19:00 +0100

    Revert "Properly close all resources on world delete / unload (#63)" (#64)
    This reverts commit edc46e0b41ce72f036097d36ae43e15b360fee3f.

--- src/main/java/xyz/nucleoid/fantasy/RuntimeWorldManager.java
+++ src/main/java/xyz/nucleoid/fantasy/RuntimeWorldManager.java
@@ -62,11 +62,7 @@ final class RuntimeWorldManager {
 
         if (this.serverAccess.getWorlds().remove(dimensionKey, world)) {
             ServerWorldEvents.UNLOAD.invoker().onWorldUnload(this.server, world);
-            try {
-                world.close();
-            } catch (IOException e) {
-                Fantasy.LOGGER.warn("Failed to close world resources", e);
-            }
+
             SimpleRegistry<DimensionOptions> dimensionsRegistry = getDimensionsRegistry(this.server);
             RemoveFromRegistry.remove(dimensionsRegistry, dimensionKey.getValue());
 
@@ -108,11 +104,7 @@ final class RuntimeWorldManager {
             }, true, false);
 
             ServerWorldEvents.UNLOAD.invoker().onWorldUnload(RuntimeWorldManager.this.server, world);
-            try {
-                world.close();
-            } catch (IOException e) {
-                Fantasy.LOGGER.warn("Failed to close world resources", e);
-            }
+
             SimpleRegistry<DimensionOptions> dimensionsRegistry = getDimensionsRegistry(RuntimeWorldManager.this.server);
             RemoveFromRegistry.remove(dimensionsRegistry, dimensionKey.getValue());
         }
