3e26e863000aade14651c4f5d97120b7ad4b7e90
sylv256 <contact@sylv.gay>
2026-03-30 15:35:16 -0400

    refactor: deduplicate mixin wrap ops

--- src/main/java/xyz/nucleoid/fantasy/mixin/TimeCommandMixin.java
+++ src/main/java/xyz/nucleoid/fantasy/mixin/TimeCommandMixin.java
@@ -16,48 +16,16 @@ import xyz.nucleoid.fantasy.RuntimeServerClockManager;
 
 @Mixin(TimeCommand.class)
 public class TimeCommandMixin {
-    @WrapOperation(method = "suggestTimeMarkers", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;clockManager()Lnet/minecraft/world/clock/ServerClockManager;"))
-    private static ServerClockManager suggestRunTimeMarkers(MinecraftServer instance, Operation<ServerClockManager> original, @Local(argsOnly = true) Holder<WorldClock> clock) {
+    @WrapOperation(
+            method = {"suggestTimeMarkers", "queryTime", "setTotalTicks", "addTime", "setTimeToTimeMarker", "setPaused", "setRate"},
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;clockManager()Lnet/minecraft/world/clock/ServerClockManager;")
+    )
+    private static ServerClockManager useRuntimeClockManager(MinecraftServer instance, Operation<ServerClockManager> original, @Local(argsOnly = true) Holder<WorldClock> clock) {
         return getClockManager(instance, original, clock);
     }
 
-    @WrapOperation(method = "queryTime", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;clockManager()Lnet/minecraft/world/clock/ServerClockManager;"))
-    private static ServerClockManager queryRunTime(MinecraftServer instance, Operation<ServerClockManager> original, @Local(argsOnly = true) Holder<WorldClock> clock) {
-        return getClockManager(instance, original, clock);
-    }
-
-    @WrapOperation(method = "queryTimelineTicks", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;clockManager()Lnet/minecraft/world/clock/ServerClockManager;"))
-    private static ServerClockManager queryRunTimelineTicks(MinecraftServer instance, Operation<ServerClockManager> original, @Local(argsOnly = true, ordinal = 0) Holder<WorldClock> clock) {
-        return getClockManager(instance, original, clock);
-    }
-
-    @WrapOperation(method = "queryTimelineRepetitions", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;clockManager()Lnet/minecraft/world/clock/ServerClockManager;"))
-    private static ServerClockManager queryRunTimelineRepetitions(MinecraftServer instance, Operation<ServerClockManager> original, @Local(argsOnly = true, ordinal = 0) Holder<WorldClock> clock) {
-        return getClockManager(instance, original, clock);
-    }
-
-    @WrapOperation(method = "setTotalTicks", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;clockManager()Lnet/minecraft/world/clock/ServerClockManager;"))
-    private static ServerClockManager setRunTotalTicks(MinecraftServer instance, Operation<ServerClockManager> original, @Local(argsOnly = true) Holder<WorldClock> clock) {
-        return getClockManager(instance, original, clock);
-    }
-
-    @WrapOperation(method = "addTime", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;clockManager()Lnet/minecraft/world/clock/ServerClockManager;"))
-    private static ServerClockManager addTime(MinecraftServer instance, Operation<ServerClockManager> original, @Local(argsOnly = true) Holder<WorldClock> clock) {
-        return getClockManager(instance, original, clock);
-    }
-
-    @WrapOperation(method = "setTimeToTimeMarker", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;clockManager()Lnet/minecraft/world/clock/ServerClockManager;"))
-    private static ServerClockManager setTimeToTimeMarker(MinecraftServer instance, Operation<ServerClockManager> original, @Local(argsOnly = true) Holder<WorldClock> clock) {
-        return getClockManager(instance, original, clock);
-    }
-
-    @WrapOperation(method = "setPaused", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;clockManager()Lnet/minecraft/world/clock/ServerClockManager;"))
-    private static ServerClockManager setPaused(MinecraftServer instance, Operation<ServerClockManager> original, @Local(argsOnly = true) Holder<WorldClock> clock) {
-        return getClockManager(instance, original, clock);
-    }
-
-    @WrapOperation(method = "setRate", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;clockManager()Lnet/minecraft/world/clock/ServerClockManager;"))
-    private static ServerClockManager setRate(MinecraftServer instance, Operation<ServerClockManager> original, @Local(argsOnly = true) Holder<WorldClock> clock) {
+    @WrapOperation(method = {"queryTimelineTicks", "queryTimelineRepetitions"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;clockManager()Lnet/minecraft/world/clock/ServerClockManager;"))
+    private static ServerClockManager useRuntimeClockManagerByOrdinal(MinecraftServer instance, Operation<ServerClockManager> original, @Local(argsOnly = true, ordinal = 0) Holder<WorldClock> clock) {
         return getClockManager(instance, original, clock);
     }
 
